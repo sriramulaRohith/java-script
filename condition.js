67e5cb533303b49f40d53bff214d70f9f55be232
Rohith Sriramula <116960651+sriramulaRohith@users.noreply.github.com>
2022-11-24 11:34:32 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,13 @@
+//factorial
+let number= prompt("enter the positive number");
+console.log(number);
+//check the number
+if(number>0){
+    console.log("yes this is positive number");
+}
+else if(number == 0){
+    console.log("the factorial is zero");
+}
+else{
+    console.log("this is negative number");
+}
---
+++ variablesscript.js
@@ -0,0 +1,38 @@
+/*console.log("hello world")
+var y=15;
+console.log("y value is",y);
+y=20;
+console.log(y);
+break*/
+
+/*let x=20;
+console.log(x);
+x=39;
+console.log(x);*/
+
+//const z=40;
+//console.log(z);
+//z=20;
+//console.log(z);//
+var i=20;
+console.log("i value is",i);
+{
+    var i=50;
+    console.log(i);
+}
+console.log("i value is",i);
+//let
+let x=50;
+console.log("x value is",x);
+{
+    let x=20;
+    console.log(x)
+   
+}
+console.log("x value is",x);
+//const
+const z=9944;
+{
+    const z=299;
+}
+console.log("z value is",z);
