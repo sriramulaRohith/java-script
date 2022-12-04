4d2fa5978946b8773aec26c3bd3b479020b204c2
Rohith Sriramula <116960651+sriramulaRohith@users.noreply.github.com>
2022-12-04 09:13:06 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,34 @@
+var name0= 'rohith' ;
+var name1= "lahari" ;
+console.log('my first name is ${name0} and my second name is ${name1}');
+console.log('My java srit id going "on"');
+console.log('Line1 \n line2');
+
+
+
+//js functions
+let text1 = 'hello';
+let text2 = 'world';
+
+const text3 ='        javasript      is main language' ;
+  //concat method
+   let resultstring =text1.concat('  ',text2);
+  // console.log(resultstring);
+
+  //convert given string into upper case
+  let resultStringUpperCase = text2.toLocaleUpperCase();
+  console.log(' resulstring is ${resultStringUpperCase}');
+  //trim()
+  console.log(text3);
+  let trimResultenValue = text3.trim();
+  console.log(trimResultenValue);
+
+
+  //split()
+  let splitResultenValue = text1.split();
+  console.log(splitResultenValue);
+
+  //slice(
+    let sliceResultenValue = text1.slice(0,8);
+console.log(sliceResultenValue);
+  
